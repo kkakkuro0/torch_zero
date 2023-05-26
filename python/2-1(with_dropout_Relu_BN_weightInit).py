cb7ccdfca32885883aa3f22ae9daa18b5f8a6105
kkakkuro0 <kkakkuro0@gmail.com>
2023-05-26 20:51:19 +0900

    5/26

---
+++ python/2-1(with_dropout_Relu_BN_weightInit).py
@@ -0,0 +1,134 @@
+# 모듈 임포트하기
+import numpy as np
+import matplotlib.pyplot as plt
+import torch
+import torch.nn as nn 
+import torch.nn.functional as F
+from torchvision import transforms, datasets
+
+# 장비확인
+if torch.cuda.is_available():
+    DEVICE = torch.device('cuda')
+else :
+    DEVICE = torch.device('cpu')
+
+print("device: ", DEVICE,", torch: ", torch.__version__)
+
+BATCH_SIZE = 32
+EPOCHS = 30
+
+# 데이터 로드
+train_dataset = datasets.MNIST(root="../data/MNIST",
+                               train=True,
+                               download=True,
+                               transform=transforms.ToTensor())
+
+test_dataset = datasets.MNIST(root="../data/MNIST",
+                               train=False,
+                               transform=transforms.ToTensor())
+
+train_loader = torch.utils.data.DataLoader(dataset = train_dataset,
+                                           batch_size = BATCH_SIZE,
+                                           shuffle = True)
+
+test_loader = torch.utils.data.DataLoader(dataset = test_dataset,
+                                           batch_size = BATCH_SIZE,
+                                           shuffle = False)
+
+# 데이터 확인 1
+for (X_train, y_train) in train_loader:
+    print("X_train: ", X_train.size(), " type: ", X_train.type())
+    print("y_train: ", y_train.size(), " type: ", y_train.type())
+    break
+
+# 데이터 확인 2
+pltsize=1
+plt.figure(figsize=(10*pltsize,pltsize))
+for i in range(10):
+    plt.subplot(1,10,i+1)
+    plt.axis('off')
+    plt.imshow(X_train[i,:,:,:].numpy().reshape(28,28), cmap="gray_r")
+    plt.title("Class: "+str(y_train[i].item()))
+# plt.show()
+
+# 모델 설계하기
+class Net(nn.Module):
+    def __init__(self):
+        super(Net,self).__init__()
+        self.fc1 = nn.Linear(28*28,512)
+        self.fc2 = nn.Linear(512,256)
+        self.fc3 = nn.Linear(256,10)
+        self.dropout_prob = 0.5
+        self.batch_norm1 = nn.BatchNorm1d(512)
+        self.batch_norm2 = nn.BatchNorm1d(256)
+
+    def forward(self,x):
+        x = x.view(-1, 28*28)
+        x = self.fc1(x)
+        x = self.batch_norm1(x)
+        x = F.relu(x)
+        x = F.dropout(x, training=self.training, p=self.dropout_prob)
+        x = self.fc2(x)
+        x = self.batch_norm2(x)
+        x = F.relu(x)
+        x = F.dropout(x, training=self.training, p=self.dropout_prob)
+        x = self.fc3(x)
+        x = F.log_softmax(x, dim=1)
+        return x
+    
+# optimizer, objective function 정의
+import torch.nn.init as init
+def weight_init(m):
+    if isinstance(m,nn.Linear):
+        init.kaiming_uniform_(m.weight.data)
+
+model = Net().to(DEVICE)
+model = model.apply(weight_init)
+optimizer = torch.optim.SGD(model.parameters(), lr=0.01, momentum=0.5)
+criterion = nn.CrossEntropyLoss()
+
+print(model)
+
+# 학습함수
+def train(model,train_loader,optimizer,log_interval):
+    model.train()
+    for batch_idx,(image,label) in enumerate(train_loader):
+        image = image.to(DEVICE)
+        label = label.to(DEVICE)
+        optimizer.zero_grad()
+        output = model(image)
+        loss = criterion(output,label)
+        loss.backward()
+        optimizer.step()
+
+        if batch_idx % log_interval == 0:
+            print("Train Epochs: {} [{}/{}({:.0f}%)]\tTrain Loss: {:.6f}".format(
+                Epoch, batch_idx*len(image),
+                len(train_loader.dataset),100.*batch_idx/len(train_loader),
+                loss.item()))
+            
+# 모델 성능확인하는 함수
+def evaluate(model, test_loader):
+    model.eval()
+    test_loss = 0
+    correct = 0
+
+    with torch.no_grad():
+        for image, label in test_loader:
+            image = image.to(DEVICE)
+            label = label.to(DEVICE)
+            output = model(image)
+            test_loss += criterion(output,label)
+            prediction = output.max(1,keepdim=True)[1]
+            correct += prediction.eq(label.view_as(prediction)).sum().item()
+
+    test_loss /= len(test_loader.dataset)
+    test_accuracy = 100. * correct / len(test_loader.dataset)
+    return test_loss, test_accuracy
+
+# 학습진행
+for Epoch in range(1, EPOCHS+1):
+    train(model,train_loader,optimizer,log_interval=200)
+    test_loss, test_accuracy = evaluate(model,test_loader)
+    print("\n[EPOCH: {}], \tTest Loss: {:.4f}, \tTest Accuracy: {:.2f} %\n".format(
+        Epoch, test_loss, test_accuracy))
